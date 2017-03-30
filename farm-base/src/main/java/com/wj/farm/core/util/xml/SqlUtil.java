37e8aa6c7e03aea98d26e2a48d99f6d70537d960
16060834 <16060834@CNHQ-16060834N.sn.suning.ad>
2017-03-30 11:53:50 +0800

    djw 引入log4j

--- farm-base/src/main/java/com/wj/farm/core/util/xml/SqlUtil.java
+++ farm-base/src/main/java/com/wj/farm/core/util/xml/SqlUtil.java
@@ -5,6 +5,8 @@ import java.util.Iterator;
 import java.util.Map;
 import java.util.Map.Entry;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
@@ -21,10 +23,14 @@ import org.springframework.util.CollectionUtils;
 @Lazy(false)
 @Component
 public class SqlUtil implements InitializingBean {
+    Logger logger = LoggerFactory.getLogger(SqlUtil.class);
+    
     private static Map<String, Map<String, String>> keySqlMap = new HashMap<String, Map<String,String>>();
     
     private static Map<String, String> proMap = new HashMap<String, String>();
     public void afterPropertiesSet() throws Exception {
+        long startTime=System.currentTimeMillis();//记录开始时间
+        logger.info("------------加载sql配置文件开始-----------");
         proMap = PropertyReader.readPropertyFile();//读取sql文件的总配置
         
         if(!CollectionUtils.isEmpty(proMap)){
@@ -39,6 +45,8 @@ public class SqlUtil implements InitializingBean {
                 keySqlMap.put(entry.getValue(), sqlMap);//properties配置文件key为主键，value存对应的sqlmap
             }
         }
+        long endTime=System.currentTimeMillis();//记录结束时间
+        logger.info("------------加载sql配置文件成功,耗时："+(endTime-startTime)+"ms-----------");
     }
     
     /**
